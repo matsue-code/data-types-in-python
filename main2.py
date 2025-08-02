8fcd7439e7bd6d7e60d25a71f143748aa9c19e86
matsue-code <mbolamathew@icloud.com>
2025-08-02 22:42:25 +0300

    second commit

---
+++ main2.py
@@ -0,0 +1,11 @@
+print("enter your name")
+name=input("enter your name:")
+print("welcome",name,"to our class today")
+x=6
+y=15
+sum=x+y
+print("the sum of",x,"and",y,"is",sum)
+x="6"
+y="15"
+sum=x+y
+print("the sum of",x,"and",y,"is",sum)
