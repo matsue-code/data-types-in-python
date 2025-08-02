a75fef1e1c6b28796c7e8bb546c5536d0c5055fd
matsue-code <mbolamathew@icloud.com>
2025-08-02 22:27:51 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+country="kenya"
+age=56
+weight=69.7
+is_student=True
+print("i live in",country)
+print("i am",age,"years old")
+print("i weigh",weight,"kg")
+print("Shawn say that he is not a student")
+print("data type of country is",type(country))
+print("data type of age is",type(age))
+print("data type of weight is",type(weight))
+print("data type of is_student is",type(is_student))
+
+
+
